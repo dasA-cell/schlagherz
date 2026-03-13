4af06b5328af80245c78e85348ce26f67d28cd57
dasA-cell <das_A@gmx.de>
2026-03-13 19:03:55 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE='schlagherz-pwa-v9-cache';
+const CACHE='schlagherz-pwa-v10-cache';
 const APP_SHELL=[
   './',
-  './index.html?v=9',
-  './manifest.webmanifest?v=9',
-  './icon-192.svg?v=9',
-  './icon-512.svg?v=9'
+  './index.html?v=10',
+  './manifest.webmanifest?v=10',
+  './icon-192.svg?v=10',
+  './icon-512.svg?v=10'
 ];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim();});
@@ -15,6 +15,6 @@ self.addEventListener('fetch',e=>{
       const copy=r.clone();
       caches.open(CACHE).then(c=>c.put(e.request,copy));
       return r;
-    }).catch(()=>caches.match('./index.html?v=9')))
+    }).catch(()=>caches.match('./index.html?v=10')))
   );
 });
