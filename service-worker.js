87ed6f6b7344fa6525d5b7d6a9db6c019b3fa976
dasA-cell <das_A@gmx.de>
2026-03-14 01:04:34 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE='schlagherz-pwa-v20-cache';
+const CACHE='schlagherz-pwa-v21-cache';
 const APP_SHELL=[
   './',
-  './index.html?v=20',
+  './index.html?v=21',
   './manifest.webmanifest?v=11',
   './icon-192.svg?v=11',
   './icon-512.svg?v=11'
@@ -15,6 +15,6 @@ self.addEventListener('fetch',e=>{
       const copy=r.clone();
       caches.open(CACHE).then(c=>c.put(e.request,copy));
       return r;
-    }).catch(()=>caches.match('./index.html?v=20')))
+    }).catch(()=>caches.match('./index.html?v=21')))
   );
 });
